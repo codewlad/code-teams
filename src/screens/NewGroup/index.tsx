7969ba0ed6b80f738e672155d3c5f96c12fb642f
Wladimir Ferreira Gonçalves <codewlad@gmail.com>
2023-08-29 20:30:53 -0300

    first commit

---
+++ src/screens/NewGroup/index.tsx
@@ -0,0 +1,59 @@
+import { useState } from 'react';
+import { Alert } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
+import { Header } from '@components/Header';
+import { Highlight } from '@components/Highlight';
+import { Button } from '@components/Button';
+import { Input } from '@components/Input';
+import { groupCreate } from '@storage/group/groupCreate';
+import { AppError } from '@utils/AppError';
+import { Container, Content, Icon } from './styles';
+
+export function NewGroup() {
+	const [group, setGroup] = useState('');
+
+	const navigation = useNavigation();
+
+	async function handleNew() {
+		try {
+			if (group.trim().length === 0) {
+				return Alert.alert('Nova turma', 'Informe o nome da turma.');
+			}
+
+			await groupCreate(group);
+			navigation.navigate('players', { group });
+		} catch (error) {
+			if (error instanceof AppError) {
+				Alert.alert('Novo turma', error.message);
+			} else {
+				Alert.alert(
+					'Nova turma',
+					'Não foi possível criar uma nova turma.'
+				);
+				console.log(error);
+			}
+		}
+	}
+
+	return (
+		<Container>
+			<Header showBackButton />
+			<Content>
+				<Icon />
+				<Highlight
+					title='Nova turma'
+					subtitle='crie a turma para adicionar as pessoas'
+				/>
+				<Input
+					placeholder='Nome da turma'
+					onChangeText={setGroup}
+				/>
+				<Button
+					title='Criar'
+					style={{ marginTop: 20 }}
+					onPress={handleNew}
+				/>
+			</Content>
+		</Container>
+	);
+}
---
+++ src/storage/storageConfig.ts
@@ -0,0 +1,4 @@
+const GROUP_COLLECTION = '@code-teams:groups';
+const PLAYER_COLLECTION = '@code-teams:players';
+
+export { GROUP_COLLECTION, PLAYER_COLLECTION };
